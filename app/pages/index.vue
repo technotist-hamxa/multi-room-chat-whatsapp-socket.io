d9105282f04e9f11b8d60fa310efc591e4fb1006
techotist-hamxa-dev <technotisthamxa.dev@gmail.com>
2025-11-09 23:31:24 +0500

    pushed

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import ChatClient from "~/components/Chat.client.vue";
-await useFetch('/api/users/createOrGetDbUser');
+const {data: user} = await useFetch('/api/users/createOrGetDbUser');
+
+onMounted(() => {
+  console.log(user);
+
+})
 </script>
 
 <template>
